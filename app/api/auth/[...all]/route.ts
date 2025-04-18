8d0cbfb4d28b760b9796dbbb3f84856894619978
Ismail Courr <ismailcourr@gmail.com>
2025-04-18 18:56:01 +0100

    Use edge

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,9 +1,6 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export let runtime = "nodejs";
-if (process.env.NODE_ENV === "production") {
-  runtime = "edge";
-}
+export const runtime = "edge";
 
 export const { GET, POST } = toNextJsHandler(auth);
