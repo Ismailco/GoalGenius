928eb3499a4ad52374535d9b9d3661d6c4c4996d
Ismail Courr <ismailcourr@gmail.com>
2025-05-10 16:02:36 +0100

    Fix the cloudflare fix

--- app/checkins/page.tsx
+++ app/checkins/page.tsx
@@ -1,8 +1,6 @@
 import CheckInsClient from './CheckInsClient';
 
 export const runtime = 'edge';
-export const preferredRegion = 'auto';
-export const dynamic = 'force-dynamic';
 
 export default function CheckInsPage() {
   return <CheckInsClient />;
