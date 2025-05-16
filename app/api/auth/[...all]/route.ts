40ebea59bbe611f5ce6abdb14a8ac8b3571fd0a1
Ismail Courr <ismailcourr@gmail.com>
2025-05-16 12:18:34 +0100

    ...

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export const { GET, POST } = toNextJsHandler(auth);
