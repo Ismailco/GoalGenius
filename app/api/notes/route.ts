e808f7533bc3bf77c46716893b3b37042b8fe82e
Ismail Courr <ismailcourr@gmail.com>
2025-06-11 18:00:15 +0100

    Use edge runtime

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -4,7 +4,7 @@ import { notes } from '@/lib/db/schema';
 import { eq } from 'drizzle-orm';
 import { v4 as uuidv4 } from 'uuid';
 
-export const runtime = "nodejs";
+export const runtime = "edge";
 
 type NoteInput = {
   userId: string;
